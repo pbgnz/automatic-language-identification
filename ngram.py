26dbe9caf450a5d0a6de7487ae74ccfe2e2e8b4e
pbgnz <pabgonza1995@gmail.com>
2018-11-28 12:32:37 -0500

    implement predict method for bigram

--- ngram.py
+++ ngram.py
@@ -10,7 +10,11 @@ from collections import Counter
 from functools import reduce
 
 
-class NGram:    
+class NGram:
+    """
+    NGram is similar to an abstract factory.
+    It is used to create Unigrams and Bigrams
+    """
     @staticmethod
     def create(degree=1, delta=0.5, name="", vocabulary=26):
         if degree == 1:
@@ -40,11 +44,10 @@ class Unigram:
         test = Corpus(test_corpus).sanitize()
         count = 1
         for line in test:
-            print(line)
             outfile = open("output/out{}.txt".format(count), "w+")
             count += 1
             tokens = []
-            french = 1
+            logTotal = 0
             outfile.write(' '.join(line)+'\n\n')
             outfile.write('UNIGRAM MODEL: {}\n\n'.format(self.corpus.name))
             tokens.append(Corpus.tokenize(line))
@@ -52,10 +55,9 @@ class Unigram:
                 for c in line:
                     outfile.write('UNIGRAM: {}\n'.format(c))
                     f = self.model[c]
-                    french += math.log10(f)
-                    outfile.write('MODEL PROBABILITY: P({}) = {:.4e} ==> log prob of sequence so far: {:.4e}\n'.format(c, f, french))
+                    logTotal += math.log10(f)
+                    outfile.write('MODEL PROBABILITY: P({}) = {:.4e} ==> log prob of sequence so far: {:.4e}\n'.format(c, f, logTotal))
             outfile.close()
-
 
     def print_model(self, ngram):
         outfile = open("output/{}.txt".format(self.name), "w")
@@ -83,7 +85,8 @@ class Bigram:
         self.corpus = [] # train corpus
         self.tokenized = [] # tokenized corpus
         self.count = [] # Counter
-        self.test_set = []
+        self.pairs = [(a,b) for a in string.ascii_lowercase for b in string.ascii_lowercase]
+        self.model = dict({el:0 for el in self.pairs})
     
     def train(self, training_corpus):
         self.corpus = Corpus(training_corpus)
@@ -92,9 +95,27 @@ class Bigram:
         self.count = Counter([(self.tokenized[i],self.tokenized[i+1]) for i in range(0,len(self.tokenized)-1)])
         self.print_model(self.count)
 
-    # def predict(self, test_corpus):
-    #     test = Corpus(test_corpus).cleans()
-
+    def predict(self, test_corpus):
+        test = Corpus(test_corpus).sanitize()
+        count = 1
+        for line in test:
+            outfile = open("output/out{}.txt".format(count), "w+")
+            tokens = []
+            logTotal = 0
+            outfile.write(' '.join(line)+'\n\n')
+            outfile.write('BIGRAM MODEL: {}\n\n'.format(self.corpus.name))
+            tokens.append(Corpus.tokenize(line))
+            tmp = tokens[0]
+            count += 1
+            if len(tmp) % 2 != 0:
+                tmp = tmp[:-1]
+            pairs = [tmp[i]+tmp[i+1] for i in range(0,len(tmp),2)]
+            for p in pairs:
+                outfile.write('BIGRAM: ({}|{})\n'.format(p[0],p[1]))
+                f = self.model[(p[0],p[1])]
+                logTotal += math.log10(f)
+                outfile.write('MODEL PROBABILITY: P({}|{}) = {:.4e} ==> log prob of sequence so far: {:.4e}\n'.format(p[1], p[0], f, logTotal))
+            outfile.close()
 
     def print_model(self, ngram):
         character_count = dict({el:0 for el in string.ascii_lowercase})
@@ -104,49 +125,16 @@ class Bigram:
                 character_count[c] = letter_frequency + self.vocabulary * self.delta
             else:
                 character_count[c] = self.vocabulary
-        pairs = [(a,b) for a in string.ascii_lowercase for b in string.ascii_lowercase]
         outfile = open("output/{}.txt".format(self.name), "w")
-        for pair in pairs:
+        for pair in self.pairs:
             pair_frequency = ngram.get(pair)
             if pair_frequency is not None:
                 pair_frequency += pair_frequency + self.delta
             else:
                 pair_frequency = self.delta
-            outfile.write("P({}|{}) = {:.4e}\n".format(pair[0], pair[1], Decimal(pair_frequency/character_count[pair[0]])))
+            self.model[pair] = Decimal(pair_frequency/character_count[pair[0]])
+            outfile.write("P({}|{}) = {:.4e}\n".format(pair[1], pair[0], self.model[pair]))
         outfile.close()
 
-
-
-bigram = NGram.create(degree=1,name="unigramEN", delta=0.5)
-bigram.train("corpora/fr-vingt-mille-lieues-sous-les-mers.txt")
-bigram.predict("corpora/first10TestSentences.txt")
-
-# test = Corpus('corpora/first10TestSentences.txt').cleans()
-# count = 1
-# for line in test:
-#     print(line)
-#     outfile = open("output/out{}.txt".format(count), "w+")
-#     count += 1
-#     tokens = []
-#     french = 1
-#     english = 1
-#     spanish = 0
-#     outfile.write(' '.join(line)+'\n\n')
-#     outfile.write('UNIGRAM MODEL:\n\n')
-#     tokens.append(tokenize(line))
-#     for line in tokens:
-#         for c in line:
-#             outfile.write('UNIGRAM: {}\n'.format(c))
-#             f = Decimal(fr.get(c)/len(tokensFR))
-#             e = Decimal(en.get(c)/len(tokensEN))
-#             french += math.log10(f)
-#             english += math.log10(e)
-#             outfile.write('FRENCH: P({}) = {:.4e} ==> log prob of sequence so far: {:.4e}\n'.format(c, f, french))
-#             outfile.write('ENGLISH: P({}) = {:.4e} ==> log prob of sequence so far: {:.4e}\n\n'.format(c, e, english))
-#     if (-french > -english) and (-french > -spanish):
-#         outfile.write('According to the unigram model, the sentence is in French')
-#     elif (-english > -french) and (-english > -spanish):
-#         outfile.write('According to the unigram model, the sentence is in English')
-#     else:
-#         outfile.write('According to the unigram model, the sentence is in Spanish')
-#     outfile.close()
+    def get_model(self):
+        return self.model
